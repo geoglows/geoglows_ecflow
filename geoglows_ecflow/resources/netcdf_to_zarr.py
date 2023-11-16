2019395d4dcf1ede75e1f0089730fcf1fa3c58a5
Michael Souffront <17328135+msouff@users.noreply.github.com>
2023-11-16 09:32:30 -0700

    fixed minor bugs

--- geoglows_ecflow/resources/netcdf_to_zarr.py
+++ geoglows_ecflow/resources/netcdf_to_zarr.py
@@ -21,47 +21,47 @@ def netcdf_forecasts_to_zarr(workspace: str) -> None:
         rapid_output = data["output_dir"]
         date = data["date"]
 
-        # Get list of forecast files
-        forecast_qout_list = glob.glob(
-            os.path.join(rapid_output, f"Qout_*.nc")
-        )
+    # Get list of forecast files
+    forecast_qout_list = glob.glob(
+        os.path.join(rapid_output, f"Qout_*.nc")
+    )
 
-        vpu_nums = sorted(
-            set(
-                [os.path.basename(x).split("_")[1] for x in forecast_qout_list]
-            )
+    vpu_nums = sorted(
+        set(
+            [os.path.basename(x).split("_")[1] for x in forecast_qout_list]
         )
+    )
 
-        def _concat_vpu_forecasts(vpu) -> xr.Dataset:
-            return xr.concat(
-                [
-                    xr.open_dataset(x).drop_vars(
-                        ["crs", "lat", "lon", "time_bnds", "Qout_err"]
+    def _concat_vpu_forecasts(vpu) -> xr.Dataset:
+        return xr.concat(
+            [
+                xr.open_dataset(x).drop_vars(
+                    ["crs", "lat", "lon", "time_bnds", "Qout_err"]
+                )
+                for x in sorted(
+                    glob.glob(
+                        os.path.join(rapid_output, f"Qout_{vpu}_*.nc")
                     )
-                    for x in sorted(
-                        glob.glob(
-                            os.path.join(rapid_output, f"Qout_{vpu}_*.nc")
-                        )
-                    )
-                ],
-                pd.Index(list(range(1, 53)), name="ensemble"),
-            )
-
-        all_vpu_ds = xr.concat(
-            [_concat_vpu_forecasts(vpu) for vpu in vpu_nums],
-            dim="rivid",
-            coords="nested",
+                )
+            ],
+            pd.Index(list(range(1, 53)), name="ensemble"),
+            fill_value=np.nan,
         )
 
-        (
-            all_vpu_ds.chunk(
-                {"time": -1, "rivid": "auto", "ensemble": -1}
-            ).to_zarr(
-                os.path.join(rapid_output, f"Qout_{date}.zarr"),
-                consolidated=True,
-                fill_value=np.nan,
-            )
+    all_vpu_ds = xr.combine_nested(
+        [_concat_vpu_forecasts(vpu) for vpu in vpu_nums],
+        concat_dim="rivid",
+        fill_value=np.nan,
+    )
+
+    (
+        all_vpu_ds.chunk(
+            {"time": -1, "rivid": "auto", "ensemble": -1}
+        ).to_zarr(
+            os.path.join(rapid_output, f"Qout_{date}.zarr"),
+            consolidated=True,
         )
+    )
 
 
 if __name__ == "__main__":
